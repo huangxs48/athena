5cf25f70fc0d3b5bfba7c39ee3eac838e99d72d1
tg851577 <tg851577@login4.stampede3.tacc.utexas.edu>
2026-07-05 21:41:16 -0500

    external source energy only update kinetic part

--- src/pgen/tde_mdot_cart.cpp
+++ src/pgen/tde_mdot_cart.cpp
@@ -769,26 +769,29 @@ void GeneralNewtonianPotentialCart(MeshBlock *pmb, const Real time, const Real d
 	  Real ay = coef1 * y + coef2 * dy + coef3 * y;
 	  Real az = coef1 * z + coef2 * dz + coef3 * z;
 	  
+	  //cons(IM1,k,j,i) += dt * rho * ax;
+	  //cons(IM2,k,j,i) += dt * rho * ay;
+	  //cons(IM3,k,j,i) += dt * rho * az;
+	  
+	  //cons(IEN,k,j,i) += dt * ax * rho * prim(IVX,k,j,i);
+	  //cons(IEN,k,j,i) += dt * ay * rho * prim(IVY,k,j,i) + dt * az * rho * prim(IVZ,k,j,i);
+
+	  //change to conserving internal energy
+	  //current internal energy
+	  Real etot_now = cons(IEN,k,j,i);
+	  Real ke_now = 0.5*(1.0/cons(IDN,k,j,i))*(SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i)));
+	  Real ie_now = etot_now - ke_now;
+
+	  Real im1_update = cons(IM1,k,j,i) + dt * rho * ax;
+	  Real im2_update = cons(IM2,k,j,i) + dt * rho * ay;
+	  Real im3_update = cons(IM3,k,j,i) + dt * rho * az;
+	
+	  Real ek_update = 0.5*(1.0/cons(IDN,k,j,i))*(SQR(im1_update) + SQR(im2_update) + SQR(im3_update));
 	  
 	  cons(IM1,k,j,i) += dt * rho * ax;
-	  cons(IM2,k,j,i) += dt * rho * ay;
-	  cons(IM3,k,j,i) += dt * rho * az;
-	  
-	  cons(IEN,k,j,i) += dt * ax * rho * prim(IVX,k,j,i);
-	  cons(IEN,k,j,i) += dt * ay * rho * prim(IVY,k,j,i) + dt * az * rho * prim(IVZ,k,j,i);
-
-	  // //change to conserving internal energy
-	  // //current internal energy
-	  // Real etot_now = cons(IEN,k,j,i);
-	  // Real ke_now = 0.5*(1.0/cons(IDN,k,j,i))*(SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i)));
-	  // Real ie_now = etot_now - ke_now;
-
-	  // Real im1_update = cons(IM1,k,j,i) + dt * rho * acc1;
-	  // Real im2_update = cons(IM2,k,j,i) + dt * rho * acc2;
-	  // Real im3_update = cons(IM3,k,j,i) + dt * rho * acc3;
-	
-	  // Real ek_update = 0.5*(1.0/cons(IDN,k,j,i))*(SQR(im1_update) + SQR(im2_update) + SQR(im3_update));
-	  // cons(IEN,k,j,i) = ie_now + ek_update;
+          cons(IM2,k,j,i) += dt * rho * ay;
+          cons(IM3,k,j,i) += dt * rho * az;
+	  cons(IEN,k,j,i) = ie_now + ek_update;
 	  
 	}
 
